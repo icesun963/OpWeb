0f4ba0b2eade9eddf6ff2351224bb2e2abd1219d
IceSun <icesun963@gmail.com>
2014-01-05 17:49:57 +0800

    port update

--- AppLauncher.js
+++ AppLauncher.js
@@ -1,21 +1,141 @@
-var connect = require('connect'),
-    http = require('http'),
-    directory = './Web';
+var fs = require('fs');
 
-var port = process.env.PORT || 5000;
-/*connect()
-    .use(connect.static(directory))
-    .listen(port);
-*/
+global.window = global;
 
-var static = require("node-static");
+var serviceConfig = function(){
+    this.Services=[];
+};
 
-var file = new static.Server('./Web');
+global.ServiceConfig = new serviceConfig();
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        file.serve(request, response);
-    }).resume();
-}).listen(port);
+global.log = require('logging');
+global.Logger = function(name){
+    this.Trace = function (msg) {
+        log(msg);
+    }
+    this.Debug = function (msg) {
+        log("{" + msg + "}");
+    }
+    this.Info = function (msg) {
+        log("[" + msg + "]");
+    }
+    this.Warn = function (msg) {
+        log("(" + msg + ")");
+    }
+    this.Error = function (msg) {
+        log("*"  + msg + "*" );
+    }
+    this.Fatal = function (msg) {
+        log("*"  + msg + "*" );
+    }
 
-console.log('Listening on port :' + port);
+}
+
+
+global.Config = require("./Config.json")
+
+
+
+log("[app start..]");
+
+global.AddService = function (app,lv) {
+    //clr class define mast use 5>= lv <= 8
+    //app use default lv = 10
+    if(lv==undefined)
+        lv = 10
+
+    var find = null;
+    ServiceConfig.Services.forEach(function(lvSvs){
+        if(lvSvs.Lv==lv ){
+            find = lvSvs;
+        }
+    });
+    if(find==null){
+        find = {
+            Lv : lv ,
+            Apps : [],
+            Rets : []
+        };
+        ServiceConfig.Services.push(find);
+    }
+    find.Apps.push(app);
+};
+
+global.StartApps = function () {
+    ServiceConfig.Services.forEach(function(lvSvs){
+        lvSvs.Apps.forEach(function (app) {
+            var ret =new app();
+            lvSvs.Rets.push(ret);
+        });
+    })
+
+}
+
+
+require("./../OpWeb/OpWeb/Shared/util.js");
+var WatchJS = require("./../OpWeb/OpWeb/Shared/watch.js");
+
+global.passProperty  = [];
+global.opStrPassProperty  = [];
+
+global.watch = function(obj,callback){
+    return WatchJS.watch(obj, $$(obj).getProperties(),callback , 0);
+};
+
+
+passProperty.push("PropertyChanged");
+
+
+
+log("[Load Services..]");
+
+//Load Services 
+
+
+var root_path = __dirname + "";
+
+function getAllFiles(root) {
+    var res = [], files = fs.readdirSync(root);
+    files.forEach(function (file) {
+        var pathname = root + '/' + file
+            , stat = fs.lstatSync(pathname);
+
+        if (!stat.isDirectory()) {
+            res.push(pathname.replace(root_path, '.'));
+        } else {
+            res = res.concat(getAllFiles(pathname));
+        }
+    });
+
+    return res
+}
+
+
+
+var files = getAllFiles(root_path);
+files.forEach(function (file) {
+
+    if (file.startsWith("./Service") && file.endsWith(".js")
+        && file.indexOf("web") == -1
+        && file.indexOf("client") == -1
+        && file.indexOf("Lib") == -1
+        ) {
+        log("[Load:" + file + "]");
+        require(file);
+    }
+});
+
+log("[Start Apps...]");
+
+
+
+StartApps();
+
+
+
+
+
+
+
+
+
