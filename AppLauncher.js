0a40a0ee83d78b52d19533d52b68173af76611e2
IceSun <icesun963@gmail.com>
2014-01-05 17:42:10 +0800

    port update

--- AppLauncher.js
+++ AppLauncher.js
@@ -9,6 +9,13 @@ var port = process.env.PORT || 5000;
 */
 
 var static = require("node-static");
-new static.Server( "./Web/" ,{ cache: false }).listen(port);;
+
+var file = new static.Server('./Web');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(port);
 
 console.log('Listening on port :' + port);
