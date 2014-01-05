f4647a0e75c6e66cb340d26a5a1f9cc76aa59284
IceSun <icesun963@gmail.com>
2014-01-05 16:40:32 +0800

    port update

--- Service/Core/OpLogService.js
+++
@@ -1,8 +0,0 @@
-var OpLogService = require("./../../Shared/OpLogRPC.js");
-
-AddService(OpLogService);
-
-AddService(function () {
-    var port = process.env.PORT || 8000;
-    CometServer(__dirname  + "/../../Web/",port);
-});
